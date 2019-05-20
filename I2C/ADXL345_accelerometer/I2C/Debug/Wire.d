6a2e28cfc2a9ed33e4a676430a3cd939a9874e0f
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2019-05-20 20:01:48 +0200

    Update repository

---
+++ I2C/ADXL345_accelerometer/I2C/Debug/Wire.d
@@ -0,0 +1,15 @@
+Wire.d Wire.o: .././Wire.c .././Wire.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
+
+.././Wire.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- I2C/ADXL345_accelerometer/I2C/main.c
+++ I2C/ADXL345_accelerometer/I2C/main.c
@@ -5,15 +5,12 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #define DEVICE (0x53) // Device address as specified in data sheet
-uint8_t _buff[6];
+uint8_t _buff[1];
+uint8_t _buff2[2];
 char POWER_CTL = 0x2D; //Power Control Register
 char DATA_FORMAT = 0x31;
 char DATAX0 = 0x32; //X-Axis Data 0
 char DATAX1 = 0x33; //X-Axis Data 1
-char DATAY0 = 0x34; //Y-Axis Data 0
-char DATAY1 = 0x35; //Y-Axis Data 1
-char DATAZ0 = 0x36; //Z-Axis Data 0
-char DATAZ1 = 0x37; //Z-Axis Data 1
 void readAccel();
 void writeTo(uint8_t address, uint8_t val) ;
 void readFrom(uint8_t address, int num, uint8_t _buff[]) ;
@@ -35,20 +32,15 @@ while(1)
 }
 
 void readAccel() {
-	uint8_t howManyuint8_tsToRead = 6;
-	readFrom( DATAX0, howManyuint8_tsToRead, _buff); //read the acceleration data from the ADXL345
+	uint8_t howManyuint8_tsToRead = 1;
+	readFrom( DATAX0, howManyuint8_tsToRead, _buff);
+	readFrom( DATAX1, howManyuint8_tsToRead, _buff2); //read the acceleration data from the ADXL345
 	// each axis reading comes in 10 bit resolution, ie 2 uint8_ts. Least Significat uint8_t first!!
 	// thus we are converting both uint8_ts in to one int
-	int x = (((int)_buff[1]) << 8) | _buff[0];
-	int y = (((int)_buff[3]) << 8) | _buff[2];
-	int z = (((int)_buff[5]) << 8) | _buff[4];
+	int x = (((int)_buff2[0]) << 8) | _buff[0];
 	printString("x: ");
 	printBinaryByte( x );
-	printString(" y: ");
-	printBinaryByte( y );
-	printString(" z: ");
-	printBinaryByte( z );
-	printString("\n");
+	
 }
 void writeTo(uint8_t address, uint8_t val) {
 	beginTransmission(DEVICE); // start transmission to device
@@ -62,7 +54,7 @@ void readFrom(uint8_t address, int num, uint8_t _buff[]) {
 	write(address); // sends address to read from
 	endTransmissionThenStop(); // end transmission
 	beginTransmission(DEVICE); // start transmission to device
-	requestFrom(DEVICE, num); // request 6 uint8_ts from device
+	requestFrom(DEVICE, num); // request num uint8_ts from device
 	int i = 0;
 	while(available()) // device may send less than requested (abnormal)
 	{
